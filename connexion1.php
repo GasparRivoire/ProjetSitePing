23ff73bcd3115bf0e70684c51c8893c60d7b9cef
RIVOIRE--GAIAO <gaspar.rg69@gmail.com>
2023-09-18 11:29:47 +0200

    Modif

--- connexion1.php
+++ connexion1.php
@@ -1,8 +1,8 @@
 <?php
     session_start();
 
-    $email =  htmlentities($_POST['email']);
-    $mdp = htmlentities($_POST['mdp']);
+    $Email =  htmlentities($_POST['Email']);
+    $Mdp = htmlentities($_POST['Mdp']);
 
     
     require_once("param.php");
@@ -15,16 +15,16 @@
     }
 
   // Attention, ici on ne vérifie pas si l'utilisateur existe déjà
-  $requete = "SELECT * FROM utilisateur WHERE email='$email'";
+  $requete = "SELECT * FROM utilisateur WHERE Email='$Email'";
   $resultat = $mysqli->query($requete);
   if (!$resultat) {
     // Erreur avec la requête
     die ("Echec !");
   } elseif ($resultat->num_rows == 0) {
-    // Requête OK, mais l'email n'a pas été trouvé (0 tuple)
+    // Requête OK, mais l'Email n'a pas été trouvé (0 tuple)
     $_SESSION['erreur'] = "Erreur de connexion";
   } else {
-    // On a trouvé un utilisateur avec la bonne adresse email :
+    // On a trouvé un utilisateur avec la bonne adresse Email :
     $tuple = $resultat->fetch_assoc();
     // Vérification du mot de passe :
     if (password_verify($password, $tuple['password'])) {
--- retour.php
+++ retour.php
@@ -1,3 +1,3 @@
-<div class="row">
+<div>
     <button class="btn btn-primary m-0" onclick="window.location.href = '<?php echo $Page;?>';" role="button">Retour</a> 
 </div>
