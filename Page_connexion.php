2e9f83a07ecd9ee9bad8b3187c4f78d15eadf6de
RIVOIRE--GAIAO <gaspar.rg69@gmail.com>
2023-09-18 09:30:10 +0200

    Modif

--- Page_connexion.php
+++ Page_connexion.php
@@ -1,7 +1,26 @@
 <?php 
 
 $titre = "Page de connexion";
-include 'Header.php';
-include 'retour.php';
+include 'C:\UwAmp\www\creation-site-internet-ping\Header.php';
+include 'C:\UwAmp\www\creation-site-internet-ping\retour.php';
 
 ?>
+<body>
+    <h2>Connexion</h2>
+
+    <div  align="center" class="row" >
+        <form method="POST" action="connexion1.php">
+
+            <label for="Email" class="form-label">Adresse email :</label>
+            <input type="Email" type="form-control" id="Email" name="Email" placeholder="Votre email..." required>
+            <br>
+            <label for="Mdp" class="form-label">Mot de passe :</label>
+            <input type="password" type="form-control" id="Mdp" name="Mdp" placeholder="Votre Mot de passe..." required>
+            <br>
+            <br>
+            <button class="btn btn-primary" type="submit">Connexion</button>
+
+        </form>
+    </div>
+
+    </body>
