044d8f57d8f6fc06388b05b7420f4e80d39a65c6
RIVOIRE--GAIAO <gaspar.rg69@gmail.com>
2023-09-24 18:18:42 +0200

    modif

---
+++ Contacts.php
@@ -0,0 +1,32 @@
+<?php 
+    $titre = "Contacts";
+    $Page = "index.html";
+    include 'Header.php';
+    include 'retour.php';
+?>
+
+<body>
+<div class="row">
+      <div class="col">
+        <div align="center" class="background-container">
+          <p class="fs-3 col-lg-5">
+            Mail de l'administration : <br><br>dgpsogk@gmail.com
+          </p>
+          <br><br><br>
+          <p class="fs-3 col-lg-5">
+            numéro de téléphone de l'administration : <br><br>06 45 85 63 52
+          </p>
+          <br>
+          
+      
+        </div>
+      </div>
+    
+          
+         
+          
+        
+          
+    </div>
+
+</body>
--- connexion1.php
+++ connexion1.php
@@ -7,33 +7,33 @@
     
     require_once("param.php");
     
-    // Connexion à la BDD
+   
     $mysqli = new mysqli($host, $login, $passwd, $dbname);
     if ($mysqli->connect_error) {
         die('Erreur de connexion (' . $mysqli->connect_errno . ') '
                 . $mysqli->connect_error);
     }
 
-  // Attention, ici on ne vérifie pas si l'utilisateur existe déjà
+
   $requete = "SELECT * FROM utilisateur WHERE Email='$Email'";
   $resultat = $mysqli->query($requete);
   if (!$resultat) {
-    // Erreur avec la requête
+
     die ("Echec !");
   } elseif ($resultat->num_rows == 0) {
-    // Requête OK, mais l'Email n'a pas été trouvé (0 tuple)
+
     $_SESSION['erreur'] = "Erreur de connexion";
   } else {
-    // On a trouvé un utilisateur avec la bonne adresse Email :
+ 
     $tuple = $resultat->fetch_assoc();
-    // Vérification du mot de passe :
+
     if (password_verify($Mdp, $tuple['Mdp'])) {
-      // Si le mot de passe est correct :
+
       $_SESSION['message'] = "Connexion réussie";
       $_SESSION['login'] = $tuple['Prénom'];
       $_SESSION['role'] = $tuple['Type'];
     }
-    else // Sinon :
+    else 
         $_SESSION['erreur'] = "Erreur de connexion";
 
   }
